bd7c21270bfd2a6a6e410e1b4eb7454f6ed15fc1
hyeond0 <a98792530@gmail.com>
2024-05-25 20:24:10 +0900

    [feat] 초기 커밋

---
+++ src/main/java/kr/co/chatpot/ChatpotApplication.java
@@ -0,0 +1,13 @@
+package kr.co.chatpot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatpotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatpotApplication.class, args);
+    }
+
+}
